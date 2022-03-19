c3fd09c9477ae8a3558b88cba2ffbadd845c9cf6
Hung Phan <phankhanhhung1718@gmail.com>
2022-03-18 21:58:21 -0400

    add customer list
    NEED HELP - App.js doesn't register ShowCustomer.js.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import RestaurantsList from "./pages/restaurant/RestaurantsList";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Restaurant from "./pages/restaurant/Restaurant";
+import ListCustomers from "./components/ListCustomers";
 
 function App() {
   return (
@@ -35,6 +36,9 @@ function App() {
         <Route exact path="/restaurants" element={<RestaurantsList />} />
 
         <Route exact path="/restaurants/:restaurant_id" element={<Restaurant />} />
+
+        <Route path="/customers" element={<ListCustomers />} />
+        <Route path="/customers/:customer_id" element={<ShowCustomer />} />
 
         {/* <Route path='/print' element={ <PrintData />} /> */}
       </Routes>
---
+++ src/components/ShowCustomer.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Spinner from 'react-bootstrap/Spinner';
+import { useNavigate, useParams } from 'react-router-dom';
+import logoPNG from "../images/qrder-logo.png";
+
+function ShowCustomer(props) {
+
+  let { id } = useParams();
+  console.log(id)
+  const [data, setData] = useState({});
+  const [showLoading, setShowLoading] = useState(true);
+
+  const apiUrl = "http://localhost:4000/api/customers/" + id;
+
+  useEffect(() => {
+    setShowLoading(false);
+    const fetchData = async () => {
+      const result = await axios(apiUrl);
+      setData(result.data);
+      setShowLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+    <img alt="Qrder Logo" className="logo" src={logoPNG} />
+
+    <div class="item-center">
+        <div>
+            {showLoading && <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span></Spinner> }    
+            <h1>Name: {data.name}</h1>
+            <p>Email: {data.email}</p>
+            <p>Phone Number {data.phoneNumber}</p>
+            <p>Date: {data.date}</p>
+            <p>Orders {data.orders}</p>
+        </div>
+    </div>
+
+  </div>
+      
+    
+  );
+}
+
+export default ShowCustomer;
